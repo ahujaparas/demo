d438f83099268cc22ae481e1a39caa01d53c4d34
paras ahuja <paras.ahuja@ril.com>
2020-05-24 17:25:26 +0530

    Changed world to India

--- model_in_production.py
+++ model_in_production.py
@@ -22,7 +22,7 @@ app = Flask(__name__)
 
 @app.route('/',methods=['POST'])
 def home():
-    return 'Hello World'
+    return 'Hello India'
 
 @app.route('/predict', methods=['POST'])
 def predict():
